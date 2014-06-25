9cc6ab96c5d3f6580077a6e8dee1ccc777761a0a
James Fish <james@fishcakez.com>
2014-06-26 00:38:45 +0100

    Add Hex support

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Dbg.Mixfile do
      name: "Dbg",
      source_url: "https://github.com/fishcakez/dbg",
      hompage_url: "https://github.com/fishcakez/dbg",
+     description: description(),
+     package: package(),
      deps: deps]
   end
 
@@ -19,4 +21,18 @@ defmodule Dbg.Mixfile do
   defp deps do
     [{:ex_doc, github: "elixir-lang/ex_doc", only: [:docs]}]
   end
+
+  defp description do
+    """
+    Distributed tracing
+    """
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md"],
+      contributors: ["James Fish"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/fishcakez/dbg"}]
+  end
+
 end
