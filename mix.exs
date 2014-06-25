eebda5ae063b917be7f308dd956ae3d2c553bc1f
James Fish <james@fishcakez.com>
2014-06-26 00:58:53 +0100

    Bump to version 0.14.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dbg.Mixfile do
 
   def project do
     [app: :dbg,
-     version: "0.14.2",
+     version: "0.14.3",
      elixir: "~> 0.14.1",
      name: "Dbg",
      source_url: "https://github.com/fishcakez/dbg",
